2602bc406d1884fb03edd055e1cbfc9ff3f0c098
Adriana R. Garmo <adrianargarmo@gmail.com>
2023-04-13 21:46:50 -0400

    Completed Lab

--- NumberAnalyzerLab/Program.cs
+++ NumberAnalyzerLab/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+Console.Write("Enter a number between 1 and 100: ");
+int number = int.Parse(Console.ReadLine());
 
-// Complete lab steps
+if (number % 2 == 1 && number < 60)
+{
+    Console.WriteLine($"{number} is odd and less than 60.");
+}
+else if (number % 2 == 0 && (number >= 2 && number <= 24))
+{
+    Console.WriteLine($"{number} is even and less than 25.");
+}
+else if (number % 2 == 0 && (number >= 26 && number <= 60))
+{
+    Console.WriteLine($"{number} is even and between 26 and 60 inclusively.");
+} 
+else if (number % 2 == 0 && number > 60)
+{
+    Console.WriteLine($"{number} is even and greater that 60.");
+}
+else if (number % 2 == 1 && number > 60)
+{
+    Console.WriteLine($"{number} is odd and greater that 60");
+}
